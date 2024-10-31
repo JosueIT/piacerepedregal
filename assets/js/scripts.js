aa6f1c5f3ced45234720b3b60dd29e5662a55f4f
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-10-31 10:12:27 -0600

    prueba2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
 function abrirPdf() {
-    window.open('https://docs.google.com/viewerng/viewer?url=https://piacerepedregal.netlify.app/assets/pdf/catalogoventadeaniversario1.pdf', '_blank');
+    window.open('https://docs.google.com/viewerng/viewer?url=https://piacerepedregal.netlify.app/assets/pdf/catalogoventadeaniversario.pdf', '_blank');
 }
-
